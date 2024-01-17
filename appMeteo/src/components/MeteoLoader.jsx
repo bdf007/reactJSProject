c0892e8f731d2ccb89ea80f9fc0bd0229f449636
bdf007 <bdf007@gmail.com>
2024-01-17 10:18:01 +0100

    add api call and error gestion

--- appMeteo/src/components/MeteoLoader.jsx
+++ appMeteo/src/components/MeteoLoader.jsx
@@ -1,19 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import loader from "../assets/loader.svg";
+import Browser from "../assets/browser.svg";
 import "./MeteoLoaderCss.css";
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function meteoLoader() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorInfo, setErrorInfo] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+      .then((response) => {
+        // console.log(response);
+        // 400 - 499: client error
+        // 500 - 599: server error
+        if (!response.ok) {
+          throw new Error(
+            `Something went wrong : Error ${response.status}, ${response.statusText}`
+          );
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        // console.log(responseData);
+        setWeatherData({
+          city: responseData.data.city,
+          country: responseData.data.country,
+          temperature: responseData.data.current.weather.tp,
+          iconId: responseData.data.current.weather.ic,
+        });
+      })
+      .catch((error) => {
+        // console.log(error);
+        // console.dir(error);
+        setErrorInfo(error.message);
+      });
+  }, []);
   return (
     <>
-      <div className="loader-container">
+      <div
+        className={`loader-container ${!weatherData && !errorInfo && "active"}`}
+      >
         <img src={loader} alt="loading icon" />
       </div>
-      <p className="city-name">Paris</p>
-      <p className="conutry-name">France</p>
-      <p className="temperature">17°C</p>
-      <div className="info-icon-container">
-        <img className="info-icon" src="/icons/01d.svg" alt="weather icon" />
-      </div>
+      {weatherData && (
+        <>
+          <p className="city-name"> {weatherData.city} </p>
+          <p className="conutry-name">{weatherData.country} </p>
+          <p className="temperature">{weatherData.temperature}°C </p>
+          <div className="info-icon-container">
+            <img
+              className="info-icon"
+              src={`/icons/${weatherData.iconId}.svg`}
+              alt="weather icon"
+            />
+          </div>
+        </>
+      )}
+
+      {errorInfo && !weatherData && (
+        <>
+          <p className="error-info">{errorInfo}</p>
+          <img src={Browser} alt="error icon" />
+        </>
+      )}
     </>
   );
 }
