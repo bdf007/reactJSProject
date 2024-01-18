305389cfb4df2b69e2e99313e40a0933aeb05742
bdf007 <bdf007@gmail.com>
2024-01-18 09:54:59 +0100

    call api add listcard and modal

--- geoData/src/App.jsx
+++ geoData/src/App.jsx
@@ -1,9 +1,42 @@
-function App() {
-  return (
-    <div>
+import React, { useState, useEffect } from "react";
+import ListCard from "./components/ListCard";
 
+function App() {
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/region/europe")
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort((a, b) => {
+          if (a.name.common < b.name.common) {
+            return -1;
+          } else if (a.name.common > b.name.common) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        setCountries(data);
+      });
+  }, []);
+  return (
+    <div className="min-h-screen bg-slate-800">
+      <div className="max-w-7x1 mx-auto py-20 px-4">
+        <h1 className="text-gray-50 text-4x1">Europe Countries Data.</h1>
+        <p className="text-gray-100 text-x1 mb-8">
+          Click on a card to reveal a country's information.
+        </p>
+        {countries && (
+          <ul className="grid min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
+            {countries.map((country, index) => (
+              <ListCard key={index} country={country} />
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- geoData/src/components/ListCard.jsx
+++ geoData/src/components/ListCard.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react"
-import { createPortal } from "react-dom"
-import ModalContent from "./ModalContent"
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import ModalContent from "./ModalContent";
 export default function ListCard({ country }) {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
       <li
-      onClick={() => setShowModal(!showModal)}
-      className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform">
+        onClick={() => setShowModal(!showModal)}
+        className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform"
+      >
         <h2 className="absolute left-0 top-0 p-2 bg-gray-50 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)] text-xl rounded">
           {country.name.common}
         </h2>
@@ -18,7 +19,14 @@ export default function ListCard({ country }) {
           alt=""
         />
       </li>
-      {showModal && createPortal(<ModalContent country={country} closeModal={() => setShowModal(!showModal)}/>, document.body)}
+      {showModal &&
+        createPortal(
+          <ModalContent
+            country={country}
+            closeModal={() => setShowModal(!showModal)}
+          />,
+          document.body
+        )}
     </>
-  )
+  );
 }
